9a7e25d90174fe4a3a23d3ce96eb4618793757c6
chiggy69 <chiragaggarwal69@gmail.com>
2022-10-12 20:58:17 +0530

    f

--- login-system/server.js
+++ login-system/server.js
@@ -12,18 +12,12 @@ app.use(
 const port = process.env.PORT || 3000;
 
 const { getAllUsers, postUser } = require("./controllers/users.controller");
-// app.set("view engine", "ejs");
-
-//load static assets
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 app.use("/", express.static(path.join(__dirname, "site")));
-//home route
-// app.get("/", (req, res) => {
-// 	res.render("base", { title: "Login System" });
-// });
+
 app.get("/users", (req, res) => {
 	res.send(getAllUsers());
 });
